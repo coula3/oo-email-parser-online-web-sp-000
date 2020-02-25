3af98e1d5c9f940e3576f0059f79e69740ae658e
Alvin Coulson <aaacoulson@gmail.com>
2020-02-25 01:22:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -5,12 +5,13 @@
 
 
 class EmailAddressParser
-attr_accessor :emails
-
-def initialize(email)
-  @emails = email
-end
-
-def parse
-  emails.delete(',').split.uniq
+  attr_accessor :emails
+  
+  def initialize(email)
+    @emails = email
+  end
+  
+  def parse
+    emails.delete(',').split.uniq
+  end
 end
